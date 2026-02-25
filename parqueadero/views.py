9f9f79de0a595c97cc1c09941891d71af03621d9
Misja117 <juanhernandezmartelo117@gmail.com>
2026-02-25 11:02:53 -0500

    cambio1

--- parqueadero/views.py
+++ parqueadero/views.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render
+
 # Create your views here.
 
 def home(request):
+    # Capacidades totales de cada parqueadero
+    central_total = 200
+    ingenieria_total = 120
+    idiomas_total = 100
+
+    # Espacios disponibles (reemplazar con datos reales de tu modelo/sensor)
+    central_disponibles = 8
+    ingenieria_disponibles = 45
+    idiomas_disponibles = 82
+
+    # Calcular porcentajes
+    central_pct = round((central_disponibles / central_total) * 100, 1) if central_total else 0
+    ingenieria_pct = round((ingenieria_disponibles / ingenieria_total) * 100, 1) if ingenieria_total else 0
+    idiomas_pct = round((idiomas_disponibles / idiomas_total) * 100, 1) if idiomas_total else 0
+
     context = {
-        'central_disponibles': 76,
-        'ingenieria_disponibles': 45,
-        'idiomas_disponibles': 36,
+        'central_disponibles': central_disponibles,
+        'central_total': central_total,
+        'central_pct': central_pct,
+        'ingenieria_disponibles': ingenieria_disponibles,
+        'ingenieria_total': ingenieria_total,
+        'ingenieria_pct': ingenieria_pct,
+        'idiomas_disponibles': idiomas_disponibles,
+        'idiomas_total': idiomas_total,
+        'idiomas_pct': idiomas_pct,
     }
     return render(request, 'home.html', context)
