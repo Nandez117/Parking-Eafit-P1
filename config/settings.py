af0f82cf49c9932a2df582312d637602b8c245f5
Misja117 <juanhernandezmartelo117@gmail.com>
2026-05-10 12:55:22 -0500

    Update ALLOWED_HOSTS with production IP

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-punzeajn!0d0uz287q9f#io5@uv2t7vzzsulf%^7q9mynbo9ba
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['52.91.109.181']
 
 
 # Application definition
