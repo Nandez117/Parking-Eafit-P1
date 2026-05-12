53902fec67547e7ac93638bd1ec54b9a44f38aef
Isabella Cadavid Posada <icadavidp@eafit.edu.co>
2026-05-12 12:08:17 -0500

    Update ALLOWED_HOSTS for production settings

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-punzeajn!0d0uz287q9f#io5@uv2t7vzzsulf%^7q9mynbo9ba
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [] 
+#ALLOWED_HOSTS = [] 
+ALLOWED_HOSTS = ['34.207.216.183'] 
 #ALLOWED_HOSTS = ['100.26.207.244'] 
 #ALLOWED_HOSTS = ['98.83.134.221'] # si lo van a correr local quiten desde ' hasta la otra '
 #ALLOWED_HOSTS = ['3.91.85.92']
